a9eba9ae662ea543a9d8fb01ceb8d9e0f890fb2b
Vanderlei Sbaraini Amancio <hello@vanderleis.me>
2019-05-24 16:31:46 -0300

    Add Cmder config

---
+++ cmder/git-for-windows-user_profile.sh
@@ -0,0 +1,8 @@
+# use this file to run your own startup commands for msys2 bash'
+
+# To add a new vendor to the path, do something like:
+# export PATH=${CMDER_ROOT}/vendor/whatever:${PATH}
+export PATH='/c/Program Files/Lando/bin':${PATH}
+
+# Uncomment this to have the ssh agent load with the first bash terminal
+# . "${CMDER_ROOT}/vendor/lib/start-ssh-agent.sh"
